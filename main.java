bd8cee358e800740b41c603dd60dfcf31e0f4727
Nahusara2021 <83132359+Nahusara2021@users.noreply.github.com>
2025-11-22 20:12:18 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+package Application;
+
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        try {
+            dbConector.conectar();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error conectando DB: " + e.getMessage());
+            return;
+        }
+
+        SwingUtilities.invokeLater(() -> new Ventana().setVisible(true));
+    }
+}
